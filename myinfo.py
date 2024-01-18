9207533cc1504913c6e379a28b4f868e41aae930
Ben <146225616+Bresker407@users.noreply.github.com>
2024-01-18 12:57:39 -0500

    Add files via upload

---
+++ myinfo.py
@@ -0,0 +1,8 @@
+#CS175L
+#Benedetto Aiello
+#myinfo
+
+print('My name is: Benedetto Aiello')
+print('My Adress is: 22 Twofort ave, Grey Gravel Co Delaware, 35173')
+print('My Telephone numer is: 1987-1986-0506')
+    
