f295026af03b8a479f2a7bfc64aa24b5940aa884
Ben <146225616+Bresker407@users.noreply.github.com>
2024-03-21 11:44:16 -0400

    Add files via upload

---
+++ Chapter 6 Average form input pt 2.py
@@ -0,0 +1,30 @@
+#Benedetto Aiello
+#CS-175-2
+#Average input pt 2
+def calculate_avg(file_name):
+    try:
+        with open (file_name, 'r') as file:
+            total = 0
+            count = 0
+            for line in file:
+                try:
+                    number = int(line.strip())
+                except ValueError:
+                    print(f"Error: Bad Input {line.strip()} skipping")
+                    continue
+                count += 1
+                total += number
+                print(f"I read in {count} number(s) Current number is: {number:8.2f} Total is: {total:8.2f}")
+
+        average = total / count if count > 0 else 0
+        print(f"Average: {average:.1f}")
+
+    except IOError:
+        print(f"SystemExit: File not found: '{file_name}'- exiting")
+
+def main():
+    file_name = 'numbers.txt'
+    calculate_avg(file_name)
+
+if __name__ == "__main__":
+    main()
